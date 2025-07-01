63343cac87417be807268c402ec0ac9272a77a14
nukkua <marco.salazar.2014.com@gmail.com>
2025-07-01 19:11:34 -0400

    it reads a file

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,19 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("Just chilling", .{});
+    // read a file in the heap
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit(); // it will check if some memory is leaked
+
+    var arena = std.heap.ArenaAllocator.init(gpa.allocator());
+    defer arena.deinit();
+
+    const arena_allocator = arena.allocator();
+    const max_bytes: usize = 500 * 2;
+
+    const file = try std.fs.cwd().openFile("test.json", .{});
+    defer file.close();
+
+    const content = try file.readToEndAlloc(arena_allocator, max_bytes);
+    std.debug.print("{s}", .{content});
 }
